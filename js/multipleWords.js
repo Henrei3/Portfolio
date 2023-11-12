aec4a4a479ae894ea2a0bd114cc117a229efa6af
Henrei3 <95311577+Henrei3@users.noreply.github.com>
2023-11-12 20:22:38 +0100

    Hole project

---
+++ js/multipleWords.js
@@ -0,0 +1,27 @@
+var buttonList = document.getElementsByTagName("button");
+
+var button = buttonList[0];
+
+var wordList = [
+'Passionné', 
+'Persévérant', 
+'Créatif',
+"Enthousiaste",
+'<a href="#About_Me"> Découvrez-moi </a>'];
+
+function changeWords(){
+let i = 0;
+var intervalId = setInterval(() => {
+     button.innerHTML = wordList[i];
+     i++; 
+     if (i === wordList.length-1){
+        clearInterval(intervalId);
+        setTimeout(()=>{
+            button.classList.add("white-button");
+            button.innerHTML = wordList[i];
+        },1000)
+     }
+}, 400);
+}
+
+setTimeout(changeWords,700);
